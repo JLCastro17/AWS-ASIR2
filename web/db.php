6b299f6d95b06c08a4bee692f1d1435222ab7a5d
JLCastro17 <51758402+JLCastro17@users.noreply.github.com>
2019-06-13 02:00:17 +0200

    Update db.php

--- web/db.php
+++ web/db.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_SERVER', 'shingeki.chbxdcsvoowl.eu-west-3.rds.amazonaws.com');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'sopde$2019');
+define('DB_PASSWORD', '**********');
 define('DB_DATABASE', 'shingeki');
 
 $connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
